506778b373ae1f7363b235392299d3e92a0ae6ce
bikas <Bikas0>
2024-01-30 00:47:36 +0600

    full code

--- trails.py
+++ trails.py
@@ -57,36 +57,36 @@ pinecone.init(api_key=PINECONE_API_KEY,
 # Creating Embeddings for Each of The Text Chunks & storing
 docsearch = Pinecone.from_texts([t.page_content for t in text_chunks], embeddings, index_name=index_name)
 
-# # If we already have an index we can load it like this
-# docsearch = Pinecone.from_existing_index(index_name, embeddings)
-#
-# prompt_template = """
-# Use the following pieces of information to answer the user's question.
-# If you don't know the answer, just say that you don't know, don't try to make up an answer.
-#
-# Context: {context}
-# Question: {question}
-#
-# Only return the helpful answer below and nothing else.
-# Helpful answer:
-# """
-#
-# PROMPT = PromptTemplate(template=prompt_template, input_variables=["context", "question"])
-# chain_type_kwargs = {"prompt": PROMPT}
-#
-# llm = CTransformers(model="model/llama-2-7b-chat.ggmlv3.q4_0.bin",
-#                     model_type="llama",
-#                     config={'max_new_tokens': 512,
-#                             'temperature': 0.8})
-#
-# qa = RetrievalQA.from_chain_type(
-#     llm=llm,
-#     chain_type="stuff",
-#     retriever=docsearch.as_retriever(search_kwargs={'k': 2}),
-#     return_source_documents=True,
-#     chain_type_kwargs=chain_type_kwargs)
-#
-# while True:
-#     user_input = input(f"Input Prompt:")
-#     result = qa({"query": user_input})
-#     print("Response : ", result["result"])
+# If we already have an index we can load it like this
+docsearch = Pinecone.from_existing_index(index_name, embeddings)
+
+prompt_template = """
+Use the following pieces of information to answer the user's question.
+If you don't know the answer, just say that you don't know, don't try to make up an answer.
+
+Context: {context}
+Question: {question}
+
+Only return the helpful answer below and nothing else.
+Helpful answer:
+"""
+
+PROMPT = PromptTemplate(template=prompt_template, input_variables=["context", "question"])
+chain_type_kwargs = {"prompt": PROMPT}
+
+llm = CTransformers(model="model/llama-2-7b-chat.ggmlv3.q4_0.bin",
+                    model_type="llama",
+                    config={'max_new_tokens': 512,
+                            'temperature': 0.8})
+
+qa = RetrievalQA.from_chain_type(
+    llm=llm,
+    chain_type="stuff",
+    retriever=docsearch.as_retriever(search_kwargs={'k': 2}),
+    return_source_documents=True,
+    chain_type_kwargs=chain_type_kwargs)
+
+while True:
+    user_input = input(f"Input Prompt:")
+    result = qa({"query": user_input})
+    print("Response : ", result["result"])
