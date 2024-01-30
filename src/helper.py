e5f70ed6603290360f34b055845ea3d5b5b40dd4
bikas <Bikas0>
2024-01-30 13:24:42 +0600

    delete

--- src/helper.py
+++ src/helper.py
@@ -16,7 +16,7 @@ def load_pdf(data):
 
 # Create text chunks
 def text_split(extracted_data):
-    text_splitter = RecursiveCharacterTextSplitter(chunk_size=500, chunk_overlap=20)
+    text_splitter = RecursiveCharacterTextSplitter(chunk_size=800, chunk_overlap=20)
     text_chunks = text_splitter.split_documents(extracted_data)
 
     return text_chunks
